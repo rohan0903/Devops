7082dbee3cfaaad1e632375e2c4751fdefcc2227
rohan0903 <50820181+rohan0903@users.noreply.github.com>
2019-12-06 11:52:38 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,14 +1,10 @@
 node {
 stage('checkout')
-git credentialsId: '939747'
-url: 'https://github.com/rohan0903/Devops.git'
-node {
-   echo 'Hello World'
-}
+git credentialsId: '8520f0a3-f733-41f6-993b-c3bf196cd7ba',url: 'https://github.com/rohan0903/Devops.git'
 stage('build/compile')
-script: 'mvn clean package'
+sh label: '',script: 'mvn clean compile package'
 stage('archive the artifacts')
 archiveArtifacts 'target/*.jar'
-stage('emailnotification')
-emailext body: 'Hi Rohan', subject: 'message', to: 'mogadampally.rohan@capgemini.com'
+stage('nexus artifact uploader')
+nexusArtifactUploader artifacts: [[artifactId: 'myproject-0.0.1-SNAPSHOT', classifier: '', file: '/var/lib/jenkins/workspace/Spring_boot_Pipeline/target/myproject-0.0.1-SNAPSHOT.jar', type: 'jar']], credentialsId: '4e4d43ee-7c26-459f-a5c2-b36a6896266e', groupId: 'com.cg', nexusUrl: '34.245.61.112:8000', nexusVersion: 'nexus3', protocol: 'http', repository: 'Springboot', version: '1.0'
 }
