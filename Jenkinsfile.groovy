8ba173bb0e7dc0784e83a1408569fb2c3379c51b
rohan0903 <50820181+rohan0903@users.noreply.github.com>
2019-12-30 17:02:40 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,44 +6,9 @@ sh label: '',script: 'mvn clean compile package'
 stage('archive the artifacts')
 archiveArtifacts 'target/*.jar'
 stage('nexus artifact uploader')
-nexusartifactUploader artifacts: [[artifactId: 'myproject-0.0.1-SNAPSHOT', classifier: '', file: '/var/lib/jenkins/workspace/Spring_boot_Pipeline/target/myproject-0.0.1-SNAPSHOT.jar', type: 'jar']], credentialsId: '4e4d43ee-7c26-459f-a5c2-b36a6896266e', groupId: 'com.cg', nexusUrl: '34.245.61.112:8000', nexusVersion: 'nexus3', protocol: 'http', repository: 'Springboot', version: '1.0'
- agent any 
-  environment {
-  PATH = "${PATH}:${getTerraformPath()}"
-}
-  stages{
-   stage('terraform init and apply -dev'){
-     steps{
-       sh  returnStatus: true, script: 'terraform workspace new dev'
-       sh "terraform init"
-       sh "terraform apply -var-file=dev.tfvars -auto-approve"
-     }
-    }
-    stage('terraform init and apply -prod'){
-     steps{
-       sh  returnStatus: true, script: 'terraform workspace new prod'
-       sh "terraform init"
-       sh "terraform apply -var-file=prod.tfvars -auto-approve"
-     }
-    }
-     stage('S3 - create bucket'){
-      steps{
-        script{
-          createS3Bucket('javahome-tf-1234')
-        }
-      }
-    }
-  }
-}
-
-def getTerraformPath() {
-  def tfHome = tool  name: 'Terraform', type: 'org.jenkinsci.plugins.terraform.TerraformInstallation'
-  return tfHome
-}
-
-def createS3Bucket(bucketName){
-  sh returnStatus: true, script: "aws s3 mb ${bucketName} --region=eu-west-1"
-}
+//nexusartifactUploader artifacts: [[artifactId: 'myproject-0.0.1-SNAPSHOT', classifier: '', file: '/var/lib/jenkins/workspace/Spring_boot_Pipeline/target/myproject-0.0.1-SNAPSHOT.jar', type: 'jar']], credentialsId: '4e4d43ee-7c26-459f-a5c2-b36a6896266e', groupId: 'com.cg', nexusUrl: '34.245.61.112:8000', nexusVersion: 'nexus3', protocol: 'http', repository: 'Springboot', version: '1.0'
+stage('jfrog artifact uploader')
+bat label: '', script: 'curl -X PUT -u u:p -T myproject-0.0.1-SNAPSHOT.jar "http://34.248.32.208:8000/artifactory/libs-release-local/myproject-0.0.1-SNAPSHOT.jar"'
 
 
     
