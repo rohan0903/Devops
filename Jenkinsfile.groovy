3568fa126a59150be0122a336e803b208a7f87ca
rohan0903 <50820181+rohan0903@users.noreply.github.com>
2019-12-31 12:55:11 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,4 +1,4 @@
-node {
+repo:node {
 stage('checkout')
 git credentialsId: '8520f0a3-f733-41f6-993b-c3bf196cd7ba',url: 'https://github.com/rohan0903/Devops.git'
 stage('build/compile')
@@ -8,8 +8,15 @@ archiveArtifacts 'target/*.jar'
 //stage('nexus artifact uploader')
 //nexusartifactUploader artifacts: [[artifactId: 'myproject-0.0.1-SNAPSHOT', classifier: '', file: '/var/lib/jenkins/workspace/Spring_boot_Pipeline/target/myproject-0.0.1-SNAPSHOT.jar', type: 'jar']], credentialsId: '4e4d43ee-7c26-459f-a5c2-b36a6896266e', groupId: 'com.cg', nexusUrl: '34.245.61.112:8000', nexusVersion: 'nexus3', protocol: 'http', repository: 'Springboot', version: '1.0'
 stage('jfrog artifact uploader')
-sh label: '', script: 'curl -X PUT -u u:p -T myproject-0.0.1-SNAPSHOT.jar "http://34.248.32.208:8000/artifactory/admin-repositories-local-SpringBoot/myproject-0.0.1-SNAPSHOT.jar"'
-}
+rtMaven.tool= 'Maven-3.6.3'
+rtMaven.Deployer releaseRepo: 'libs-release-local' , snapshotRepo: 'libs-snapshot-local' , server: server
+rtMaven.resolver releaseRepo: 'libs-release' , snapshotRepo: 'libs-snapshot' , server: server
+rtMaven.deployer.artifactDeploymentPatterns.addExclude("pom.xml")
+buildInfo = Artifactory.newBuildInfo()
+buildInfo.retention maxBuilds: 20, maxDays: 7, deleteBuildArtifacts: true
+buildInfo.env.capture = true
+}    
+    
 
     
      
